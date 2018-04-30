e1107b8bb6d87e7b1c5c67d256d3a4985075475e
Sunny <sunny.yu@nih.gov>
2018-04-29 23:45:16 -0400

    Remove sql scripts

--- server/db/insert_pubcode_row_template.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO public."pubcode_table1"(
-	"CodeTitle", "PCID", "PMID", "PMCID", "PaperPubDate", "CodePubDate", "Authors", "CodeLanguage", "OS", "Dependencies", "Licensing", "Keywords", "Dataset_IDs", "GitHub_URL", "GH_Forks", "GH_OpenIssues", "GH_ClosedIssues", "GH_DateUpdate")
-	VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
